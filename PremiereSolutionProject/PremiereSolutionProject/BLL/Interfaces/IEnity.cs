fb623aaf9bd16c28ffce7ef40415ac558cdc505a
Matin De Wet <matindewet116@gmail.com>
2021-04-27 20:38:45 +0200

    Created BLL, DAL File Structure
    interfaces added

---
+++ PremiereSolutionProject/PremiereSolutionProject/BLL/Interfaces/IEnity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremiereSolutionProject.BLL.Interfaces
+{
+    interface IEnity
+    {
+        #region Properties
+        //properties that all the inheriting classes will have
+        string Id
+        {
+            get;
+            set;
+        }
+
+        Address Address
+        {
+            get;
+            set;
+        }
+        string ContactNumber
+        {
+            get;
+            set;
+        }
+        DateTime RegistrationDate
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        ////creating a list of respective objects
+        //List<IEntity> CreateList();
+
+        ////registering a new object, (i.e. new IndividualClient, new MaintenanceEmployee)
+        //IEntity RegisterEntity();
+    }
+}
---
+++ PremiereSolutionProject/PremiereSolutionProject/DAL/Interfaces/IAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremiereSolutionProject.DAL.Interfaces
+{
+    interface IAction
+    {
+        //bool Add(object myobject);
+        //bool Update(int myidentifier, object myobject);
+        void Delete(int myidentifier);
+    }
+}
