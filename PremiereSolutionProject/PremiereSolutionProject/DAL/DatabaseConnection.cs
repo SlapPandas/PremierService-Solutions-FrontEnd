ada38d91ab08d72f4c847ed364e6b60ad7f4093e
Matin De Wet <matindewet116@gmail.com>
2021-04-27 20:51:20 +0200

    csProject

--- PremiereSolutionProject/PremiereSolutionProject/DAL/DatabaseConnection.cs
+++ PremiereSolutionProject/PremiereSolutionProject/DAL/DatabaseConnection.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using PremiereSolutionProject.DAL.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PremiereSolutionProject.DAL
 {
-    class DatabaseConnection
+    class DatabaseConnection : IDataconnection
     {
+        #region Fields
+        public string connectionSring = @"Data Source =.; Initial Catalog = PremierServiceSolutionsDatabase; Integrated Security = SSPI";
+        public SqlConnection Connection;
+
+        public string commandString = "";
+        public SqlCommand Command;
+
+        public SqlDataReader Reader;
+        #endregion
+
+        #region Methods
+        public void CreateConnection()
+        {
+            Connection = new SqlConnection(connectionSring);
+        }
+        public void OpenConnection()
+        {
+            try
+            {
+                Connection.Open();
+            }
+            catch (Exception e)
+            {
+                DatabaseOperationDH databaseOperationDH = new DatabaseOperationDH();
+                DatabaseOperation databaseOperation = new DatabaseOperation(false, e.ToString());
+                databaseOperationDH.CreateOperationLog(databaseOperation);
+            }
+
+        }
+        public void CloseConnection()
+        {
+            try
+            {
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                DatabaseOperationDH databaseOperationDH = new DatabaseOperationDH();
+                DatabaseOperation databaseOperation = new DatabaseOperation(false, e.ToString());
+                databaseOperationDH.CreateOperationLog(databaseOperation);
+            }
+
+        }
+        #endregion
     }
 }
