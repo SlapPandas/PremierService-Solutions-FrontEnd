02472dd3055bd93ebc395bef6ef174207b745c70
Blessed <77158839+BlessedRap@users.noreply.github.com>
2021-05-03 21:24:42 +0200

    Added in the error forms as requested
    Worked on the error form and the implementation of the "Update" in job management.

---
+++ PremiereSolutionProject/PremiereSolutionProject/PL/frmErrorsTesting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PremiereSolutionProject.PL
+{
+    public partial class frmErrorsTesting : Form
+    {
+        public frmErrorsTesting()
+        {
+            InitializeComponent();
+        }
+    }
+}
