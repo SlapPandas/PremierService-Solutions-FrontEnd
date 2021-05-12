85ca53071442d02df1f41e58eab7a89341489fe5
Lijani <lijanivwdv@gmail.com>
2021-05-12 11:35:26 +0200

    Updated frmContractManagement
    Added 2 buttons
    font changes
    anchoring

--- PremiereSolutionProject/PremiereSolutionProject/PL/frmContractManagement.Designer.cs
+++ PremiereSolutionProject/PremiereSolutionProject/PL/frmContractManagement.Designer.cs
@@ -40,73 +40,80 @@ namespace PremiereSolutionProject.PL
             this.lbxPackages = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnRemove = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvViewServices)).BeginInit();
             this.SuspendLayout();
             // 
             // btnExit
             // 
-            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Location = new System.Drawing.Point(793, 630);
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(13, 13);
             this.btnExit.Margin = new System.Windows.Forms.Padding(4);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(102, 34);
             this.btnExit.TabIndex = 32;
-            this.btnExit.Text = "Exit";
+            this.btnExit.Text = "Close";
             this.btnExit.UseVisualStyleBackColor = true;
             // 
             // btnCreateContract
             // 
-            this.btnCreateContract.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCreateContract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreateContract.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(55)))), ((int)(((byte)(84)))));
             this.btnCreateContract.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCreateContract.Location = new System.Drawing.Point(333, 542);
+            this.btnCreateContract.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateContract.Location = new System.Drawing.Point(588, 472);
             this.btnCreateContract.Margin = new System.Windows.Forms.Padding(4);
             this.btnCreateContract.Name = "btnCreateContract";
-            this.btnCreateContract.Size = new System.Drawing.Size(175, 34);
+            this.btnCreateContract.Size = new System.Drawing.Size(259, 70);
             this.btnCreateContract.TabIndex = 28;
             this.btnCreateContract.Text = "Create New Contract";
-            this.btnCreateContract.UseVisualStyleBackColor = true;
+            this.btnCreateContract.UseVisualStyleBackColor = false;
             this.btnCreateContract.Click += new System.EventHandler(this.btnCreateContract_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(107, 215);
+            this.label7.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 320);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(62, 17);
+            this.label7.Size = new System.Drawing.Size(230, 19);
             this.label7.TabIndex = 24;
-            this.label7.Text = "Services";
+            this.label7.Text = "Services in Service Package";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(107, 104);
+            this.label3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 68);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 17);
+            this.label3.Size = new System.Drawing.Size(134, 19);
             this.label3.TabIndex = 20;
             this.label3.Text = "Service package";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(327, 39);
+            this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(303, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(295, 32);
+            this.label1.Size = new System.Drawing.Size(277, 33);
             this.label1.TabIndex = 18;
             this.label1.Text = "Contract Management";
             // 
             // btnAddServicePackage
             // 
-            this.btnAddServicePackage.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnAddServicePackage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnAddServicePackage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAddServicePackage.Location = new System.Drawing.Point(494, 440);
+            this.btnAddServicePackage.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddServicePackage.Location = new System.Drawing.Point(588, 115);
             this.btnAddServicePackage.Margin = new System.Windows.Forms.Padding(4);
             this.btnAddServicePackage.Name = "btnAddServicePackage";
-            this.btnAddServicePackage.Size = new System.Drawing.Size(155, 34);
+            this.btnAddServicePackage.Size = new System.Drawing.Size(261, 40);
             this.btnAddServicePackage.TabIndex = 33;
             this.btnAddServicePackage.Text = "Add Service Package";
             this.btnAddServicePackage.UseVisualStyleBackColor = true;
@@ -114,62 +121,98 @@ namespace PremiereSolutionProject.PL
             // 
             // cbxServicePackages
             // 
+            this.cbxServicePackages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cbxServicePackages.FormattingEnabled = true;
-            this.cbxServicePackages.Location = new System.Drawing.Point(110, 149);
+            this.cbxServicePackages.Location = new System.Drawing.Point(41, 91);
             this.cbxServicePackages.Margin = new System.Windows.Forms.Padding(4);
             this.cbxServicePackages.Name = "cbxServicePackages";
-            this.cbxServicePackages.Size = new System.Drawing.Size(339, 24);
+            this.cbxServicePackages.Size = new System.Drawing.Size(806, 21);
             this.cbxServicePackages.TabIndex = 29;
             this.cbxServicePackages.SelectedIndexChanged += new System.EventHandler(this.cbxServicePackages_SelectedIndexChanged);
             // 
             // dgvViewServices
             // 
+            this.dgvViewServices.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvViewServices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvViewServices.Location = new System.Drawing.Point(110, 243);
+            this.dgvViewServices.Location = new System.Drawing.Point(43, 343);
             this.dgvViewServices.Margin = new System.Windows.Forms.Padding(4);
             this.dgvViewServices.Name = "dgvViewServices";
             this.dgvViewServices.RowHeadersWidth = 62;
-            this.dgvViewServices.Size = new System.Drawing.Size(539, 184);
+            this.dgvViewServices.Size = new System.Drawing.Size(537, 199);
             this.dgvViewServices.TabIndex = 27;
             // 
             // lbxPackages
             // 
+            this.lbxPackages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lbxPackages.FormattingEnabled = true;
-            this.lbxPackages.ItemHeight = 16;
-            this.lbxPackages.Location = new System.Drawing.Point(709, 247);
+            this.lbxPackages.Location = new System.Drawing.Point(43, 167);
             this.lbxPackages.Name = "lbxPackages";
-            this.lbxPackages.Size = new System.Drawing.Size(155, 180);
+            this.lbxPackages.Size = new System.Drawing.Size(804, 121);
             this.lbxPackages.TabIndex = 34;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(706, 215);
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 136);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.Size = new System.Drawing.Size(133, 19);
             this.label2.TabIndex = 35;
             this.label2.Text = "Added packages";
             // 
             // btnRemove
             // 
-            this.btnRemove.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRemove.Location = new System.Drawing.Point(709, 440);
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(588, 295);
             this.btnRemove.Margin = new System.Windows.Forms.Padding(4);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(155, 34);
+            this.btnRemove.Size = new System.Drawing.Size(259, 40);
             this.btnRemove.TabIndex = 36;
             this.btnRemove.Text = "Remove Service Package";
             this.btnRemove.UseVisualStyleBackColor = true;
             this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(588, 376);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 40);
+            this.button1.TabIndex = 37;
+            this.button1.Text = "Clear all fields";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(588, 424);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 40);
+            this.button2.TabIndex = 38;
+            this.button2.Text = "Cancel creation";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // frmContractManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
-            this.ClientSize = new System.Drawing.Size(908, 677);
+            this.ClientSize = new System.Drawing.Size(878, 575);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbxPackages);
@@ -205,5 +248,7 @@ namespace PremiereSolutionProject.PL
         private System.Windows.Forms.ListBox lbxPackages;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
