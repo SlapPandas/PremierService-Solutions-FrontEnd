d0297ce77e5f2f8107832ae5fc112c5a5cbbb519
Lijani <lijanivwdv@gmail.com>
2021-05-16 11:35:51 +0200

    Cleaned the code in frmBusinessClient

--- PremiereSolutionProject/PremiereSolutionProject/PL/frmBusinessClient.cs
+++ PremiereSolutionProject/PremiereSolutionProject/PL/frmBusinessClient.cs
@@ -8,23 +8,26 @@ namespace PremiereSolutionProject.PL
 {
     public partial class frmBusinessClient : Form
     {
-        List<BusinessClient> businessClients = new List<BusinessClient>();
-        BusinessClient businessClient = new BusinessClient();
-        BindingSource bindingSource = new BindingSource();
+
         public frmBusinessClient()
         {
             InitializeComponent();
         }
-        private void frmBusinessClient_Load(object sender, EventArgs e)
-        {
-            RefreshDGVAndList();
-            BuildDGVStyle();
-            PopulateComboBox();
-        }
+
+        #region Declarations
+
+        List<BusinessClient> businessClients = new List<BusinessClient>();
+        BusinessClient businessClient = new BusinessClient();
+        BindingSource bindingSource = new BindingSource();
+
+        #endregion
+
+        #region Events
         private void dgvBusinessClients_SelectionChanged(object sender, EventArgs e)
         {
             UpdateFields(dgvBusinessClients.CurrentCell.RowIndex);
         }
+
         private void btnDeleteBusinessClient_Click(object sender, EventArgs e)
         {
             DialogResult dr = MessageBox.Show("Are you sure to delete The Client?", "Confirmation", MessageBoxButtons.YesNo);
@@ -36,6 +39,7 @@ namespace PremiereSolutionProject.PL
                 UpdateFields(dgvBusinessClients.CurrentCell.RowIndex);
             }
         }
+
         private void btnUpdate_Click(object sender, EventArgs e)
         {
             DialogResult dr = MessageBox.Show("Are you sure to update The Client?", "Confirmation", MessageBoxButtons.YesNo);
@@ -47,6 +51,7 @@ namespace PremiereSolutionProject.PL
                 UpdateFields(dgvBusinessClients.CurrentCell.RowIndex);
             }
         }
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
             DialogResult dr = MessageBox.Show("Are you sure to Insert The Client?", "Confirmation", MessageBoxButtons.YesNo);
@@ -59,6 +64,112 @@ namespace PremiereSolutionProject.PL
             }
         }
 
+        private void btnClearSearch_Click(object sender, EventArgs e)
+        {
+            RefreshDGVAndList();
+        }
+
+        //TODO: need to figure out why the search is not working
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text.Length >= 9)
+            {
+                businessClients.Clear();
+                businessClients.Add(businessClient.SelectAllBusinessClientsByThereId(txtSearch.Text));
+                RefreshDGVAndListWithoutReset();
+            }
+            else
+            {
+                MessageBox.Show("Please make sure to enter an ID to search for.","No ID entered", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClearTextBoxes_Click(object sender, EventArgs e)
+        {
+            txtBusinessName.Text = "";
+            txtCity.Text = "";
+            txtContactNumber.Text = "";
+            txtPostalCode.Text = "";
+            txtSearch.Text = "";
+            txtStreetName.Text = "";
+            txtSuburb.Text = "";
+            txtTaxNum.Text = "";
+            cmbActive.SelectedIndex = -1;
+            cmbProvince.SelectedIndex = -1;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void frmBusinessClient_Load(object sender, EventArgs e)
+        {
+            RefreshDGVAndList();
+            BuildDGVStyle();
+            PopulateComboBox();
+        }
+
+        private void BuildDGVStyle()
+        {
+            dgvBusinessClients.ForeColor = Color.Black;
+        }
+
+        private void RefreshDGVAndList()
+        {
+            businessClients = businessClient.SelectAllBusinessClients();
+            List<BusinessClient> bindList = businessClients;
+            bindingSource = new BindingSource(bindList, null);
+            dgvBusinessClients.DataSource = bindingSource;
+        }
+
+        private void RefreshDGVAndListWithoutReset()
+        {
+            List<BusinessClient> bindList = businessClients;
+            bindingSource = new BindingSource(bindList, null);
+            dgvBusinessClients.DataSource = bindingSource;
+        }
+
+        private void UpdateFields(int index)
+        {
+            if (index >= businessClients.Count)
+            {
+                index = 0;
+            }
+            if (index <= dgvBusinessClients.RowCount - 2)
+            {
+                txtBusinessName.Text = businessClients[index].BusinessName;
+                txtTaxNum.Text = businessClients[index].TaxNumber;
+                txtStreetName.Text = businessClients[index].Address.StreetName;
+                txtSuburb.Text = businessClients[index].Address.Suburb;
+                txtCity.Text = businessClients[index].Address.City;
+                cmbProvince.SelectedItem = businessClients[index].Address.Province;
+                txtPostalCode.Text = businessClients[index].Address.Postalcode;
+                txtContactNumber.Text = businessClients[index].ContactNumber;
+                cmbActive.SelectedIndex = GetIntFromBool(businessClients[index].Active);
+            }
+        }
+
+        private bool GetTrueFalseFromBit(int bit)
+        {
+            bool output = bit == 1 ? true : false;
+            return output;
+        }
+        private int GetIntFromBool(bool input)
+        {
+            if (input == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
         private void PopulateComboBox()
         {
             for (int i = 0; i < 9; i++)
@@ -105,102 +216,6 @@ namespace PremiereSolutionProject.PL
             }
             return province;
         }
-        private void BuildDGVStyle()
-        {
-            dgvBusinessClients.ForeColor = Color.Black;
-        }
-        private void RefreshDGVAndList()
-        {
-            businessClients = businessClient.SelectAllBusinessClients();
-            List<BusinessClient> bindList = businessClients;
-            bindingSource = new BindingSource(bindList, null);
-            dgvBusinessClients.DataSource = bindingSource;
-        }
-        private void RefreshDGVAndListWithoutReset()
-        {
-            List<BusinessClient> bindList = businessClients;
-            bindingSource = new BindingSource(bindList, null);
-            dgvBusinessClients.DataSource = bindingSource;
-        }
-        private void UpdateFields(int index)
-        {
-            if (index >= businessClients.Count)
-            {
-                index = 0;
-            }
-            if (index <= dgvBusinessClients.RowCount - 2)
-            {
-                txtBusinessName.Text = businessClients[index].BusinessName;
-                txtTaxNum.Text = businessClients[index].TaxNumber;
-                txtStreetName.Text = businessClients[index].Address.StreetName;
-                txtSuburb.Text = businessClients[index].Address.Suburb;
-                txtCity.Text = businessClients[index].Address.City;
-                cmbProvince.SelectedItem = businessClients[index].Address.Province;
-                txtPostalCode.Text = businessClients[index].Address.Postalcode;
-                txtContactNumber.Text = businessClients[index].ContactNumber;
-                cmbActive.SelectedIndex = GetIntFromBool(businessClients[index].Active);
-            }
-
-
-        }
-        private bool GetTrueFalseFromBit(int bit)
-        {
-            bool output = bit == 1 ? true : false;
-            return output;
-        }
-        private int GetIntFromBool(bool input)
-        {
-            if (input == true)
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        private void btnClearSearch_Click(object sender, EventArgs e)
-        {
-            RefreshDGVAndList();
-        }
-
-        //TODO: need to figure out why the search is not working
-        private void btnSearch_Click(object sender, EventArgs e)
-        {
-            if (txtSearch.Text.Length >= 9)
-            {
-                businessClients.Clear();
-                businessClients.Add(businessClient.SelectAllBusinessClientsByThereId(txtSearch.Text));
-                RefreshDGVAndListWithoutReset();
-            }
-            else
-            {
-                MessageBox.Show("Please make sure to enter an ID to search for.","No ID entered", MessageBoxButtons.OK);
-            }
-        }
-
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void btnClearTextBoxes_Click(object sender, EventArgs e)
-        {
-            txtBusinessName.Text = "";
-            txtCity.Text = "";
-            txtContactNumber.Text = "";
-            txtPostalCode.Text = "";
-            txtSearch.Text = "";
-            txtStreetName.Text = "";
-            txtSuburb.Text = "";
-            txtTaxNum.Text = "";
-            cmbActive.SelectedIndex = -1;
-            cmbProvince.SelectedIndex = -1;
-        }
-
-        #region Methods
-
-
 
         #endregion
     }
